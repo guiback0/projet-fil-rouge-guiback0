09840f0d3427e2e0c10ce610e50622bada2e8dfa
guiback0 <guillaume.zimmer.pro@gmail.com>
2025-05-08 12:38:13 +0200

    🏷️  Entity : acces

--- access_mns_manager/src/Entity/Badgeuse.php
+++ access_mns_manager/src/Entity/Badgeuse.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BadgeuseRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,6 +21,17 @@ class Badgeuse
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $date_installation = null;
+
+    /**
+     * @var Collection<int, Acces>
+     */
+    #[ORM\OneToMany(targetEntity: Acces::class, mappedBy: 'badgeuse')]
+    private Collection $acces;
+
+    public function __construct()
+    {
+        $this->acces = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +58,36 @@ class Badgeuse
     public function setDateInstallation(\DateTimeInterface $date_installation): static
     {
         $this->date_installation = $date_installation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Acces>
+     */
+    public function getAcces(): Collection
+    {
+        return $this->acces;
+    }
+
+    public function addAcce(Acces $acce): static
+    {
+        if (!$this->acces->contains($acce)) {
+            $this->acces->add($acce);
+            $acce->setBadgeuse($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAcce(Acces $acce): static
+    {
+        if ($this->acces->removeElement($acce)) {
+            // set the owning side to null (unless already changed)
+            if ($acce->getBadgeuse() === $this) {
+                $acce->setBadgeuse(null);
+            }
+        }
 
         return $this;
     }
