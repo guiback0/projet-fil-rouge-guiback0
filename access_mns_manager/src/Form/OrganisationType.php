f03458b733fce37759e11b902597149d4d2b3da7
guiback0 <guillaume.zimmer.pro@gmail.com>
2025-05-18 12:34:31 +0200

    ✨Feat : organisation controller

---
+++ access_mns_manager/src/Form/OrganisationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Organisation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrganisationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_organisation')
+            ->add('telephone')
+            ->add('email')
+            ->add('site_web')
+            ->add('date_creation', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('siret')
+            ->add('ca')
+            ->add('numero_rue')
+            ->add('suffix_rue')
+            ->add('nom_rue')
+            ->add('code_postal')
+            ->add('ville')
+            ->add('pays')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Organisation::class,
+        ]);
+    }
+}
