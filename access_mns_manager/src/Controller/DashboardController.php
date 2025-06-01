970876a3e1b0cd03c68d040d4ff833e6281fc8f0
guiback0 <guillaume.zimmer.pro@gmail.com>
2025-06-01 22:02:50 +0200

    ✨ Feat: organisation and user count / recent organisation

--- access_mns_manager/src/Controller/DashboardController.php
+++ access_mns_manager/src/Controller/DashboardController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\OrganisationRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DashboardController extends AbstractController{
     #[Route('/', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, OrganisationRepository $organisationRepository): Response
     {
+        // Count total users and organisations
+        $totalUsers = $userRepository->count([]);
+        $totalOrganisations = $organisationRepository->count([]);
+        
+        // Get recent organisations (last 5)
+        $recentOrganisations = $organisationRepository->findBy([], ['date_creation' => 'DESC'], 5);
+        
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'total_users' => $totalUsers,
+            'total_organisations' => $totalOrganisations,
+            'recent_organisations' => $recentOrganisations,
         ]);
     }
 }
